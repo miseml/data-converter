4ed2e41aeebfa68a8f76001187715bceee8977cc
miseml <semla.michal@g.pl>
2023-07-26 23:21:57 -0700

    Added main method of program
    * CLI argument parser
    * Injection mechanism for serializers

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # data-converter
 
 
+
+## Serializers
+Converts file with personal details into PersonalData objects and prints it on screen.
+
+Supported formats:
+* json(builtin json lib and simplejson)
+* yaml
+
 ## Formatters
 Presents data as text or HTML
+
+## Example usage
+
+Convert yaml file into text
+
+`python main.py test_files/test.yaml yaml text`
+
+Convert json file into html
+
+`python main.py test_files/test.yaml json html`
---
+++ main.py
@@ -0,0 +1,48 @@
+
+import inject
+import argparse
+
+import formatters.html as html
+import formatters.text as text
+
+from serializers.json_serializer import JSONSerializer
+from serializers.simplejson_serializer import SimpleJSONSerializer
+from serializers.yaml_serializer import YAMLSerializer
+
+# This should be injected, it would be easier to extend functionality of a program
+DISPLAY_FORMATS = {
+    "text": text.text_formatter,
+    "html": html.html_formatter,
+}
+
+def configure(binder):
+    binder.bind("json", JSONSerializer())
+    binder.bind("simplejson", SimpleJSONSerializer())
+    binder.bind("yaml", YAMLSerializer())
+
+inject.configure(configure)
+
+def main():
+    parser = argparse.ArgumentParser(description="Serialize data from a file using different serializers and output formats.")
+    parser.add_argument("filepath", type=str, help="Path to the file.")
+    parser.add_argument("serializer", choices=["json", "simplejson", "yaml"], help="Choose the serializer (json, simplejson, or yaml).")
+    parser.add_argument("outputFormat", choices=["text", "html"], help="Choose the output format (text or html).")
+
+    args = parser.parse_args()
+
+    filepath = args.filepath
+    serializer = args.serializer
+    output_format = args.outputFormat
+
+    print("Filepath:", filepath)
+    print("Serializer:", serializer)
+    print("Output Format:", output_format)
+
+    with open(filepath, "r") as file:
+        file_content = file.read()
+        result = inject.instance(serializer).deserialize(file_content)
+        display = DISPLAY_FORMATS[output_format]
+        display(result)
+
+if __name__ == "__main__":
+    main()
